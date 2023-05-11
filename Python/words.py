aa77ab96e2d0382e3889f7326aa132974fab7c18
Cavanite <bertdezeeuw@live.nl>
2023-05-11 20:08:09 +0200

    python getting started...

---
+++ Python/words.py
@@ -0,0 +1,11 @@
+#Learning examples
+
+#URL Request + decode lines
+from urllib.request import urlopen
+story = urlopen('http://sixty-north.com/c/t.txt')
+story_words = []
+for line in story:
+    line_words = line.decode('utf8').split()
+    for word in line_words:
+        story_words.append(word)
+story.close()
