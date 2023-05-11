6f458d52c52c94d06d5d71b17bf2c08f8d4e5db5
Cavanite <bertdezeeuw@live.nl>
2023-05-11 20:15:35 +0200

    Start

--- Python/words.py
+++ Python/words.py
@@ -1,7 +1,6 @@
-#Learning examples
-
-#URL Request + decode lines
 from urllib.request import urlopen
+
+
 story = urlopen('http://sixty-north.com/c/t.txt')
 story_words = []
 for line in story:
@@ -9,3 +8,6 @@ for line in story:
     for word in line_words:
         story_words.append(word)
 story.close()
+
+for word in story_words:
+    print(word)
