0d0080991a1f0326fc8c0ed2c6c88f7f1b51f21c
cavanite <bertdezeeuw@live.nl>
2023-09-14 10:42:18 +0200

    Added Check

--- PowerShell/Office365/Intune/Install-Winget.ps1
+++ PowerShell/Office365/Intune/Install-Winget.ps1
@@ -1,4 +1,8 @@
-$progressPreference = 'silentlyContinue'
+# Check winget is installed
+if (-Not (Get-Command -Name winget -ErrorAction SilentlyContinue)) {
+    # Winget is niet geinstalleerd
+    #script by Cavanite
+   $progressPreference = 'silentlyContinue'
 Write-Information "Downloading WinGet and its dependencies..."
 Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
@@ -6,3 +10,15 @@ Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/d
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
 Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+ 
+
+    # Install succesvol geweest?
+    if ($?) {
+        Write-Host "Winget succesvol geinstalleerd"
+    } else {
+        Write-Host "Winget failed"
+    }
+} else {
+    Write-Host "Winget is al geinstalleerd"
+}
