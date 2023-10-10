adaf1b0c3e82bc56d7da4c53ed33255ca07201d9
cavanite <bertdezeeuw@live.nl>
2023-10-10 09:34:54 +0200

    Added file handle

---
+++ PowerShell/Azure/GetFileHandles-AndClose.ps1
@@ -0,0 +1,16 @@
+#Open Azure Cloud Shell
+$sharename = "xxxx"
+$subscriptionid = "xxxx-xxxx-xxxx-xxxx"
+
+#Select Subscription
+Select-AzSubscription -subscriptionid $subscriptionid
+
+$Context = New-AzStorageContext -StorageAccountName "xxxx" -StorageAccountKey "xxxx"
+
+#Now retrieve the file handles
+Get-AzStorageFileHandle -Context $Context -ShareName $sharename -Recursive
+
+#Search the path of the file handle you want to close
+
+Close-AzStorageFileHandle -Context $Context -ShareName "xxxx" -Path 'Path to file handle' -CloseAll
+
