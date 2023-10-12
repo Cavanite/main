750d2371089daaf9cf82becd50b6859e47af9721
cavanite <bertdezeeuw@live.nl>
2023-10-12 10:50:57 +0200

    change

--- PowerShell/Office365/Exchange Online/Bulk-AddMembers_distrubution-list.ps1
+++ PowerShell/Office365/Exchange Online/Bulk-AddMembers_distrubution-list.ps1
@@ -1,18 +1,22 @@
-#Connect-ExchangeOnline
-$Pathtocsv = C:\temp\Users.csv
-$targetdistrubutionlist = 'xxx'
-
-Connect-ExchangeOnline
-
-#Import-csv
-Import-Csv -path -Delimiter ';' $Pathtocsv
-
-#For each loop to add all the users to distrubution list
-
-foreach ($user in $users) {
-    #Grab Email adres from the CSV
-    $email = $user.email
-    #Add the user to the distrubution list
-    Add-DistrubutionGroupMember -Identity $targetdistrubutionlist -Member $email
-
+$GroupEmailID = "RoadandMarine@fincoenergies.com"
+$CSVFile = "C:\Temp\Users.txt"
+ 
+#Connect to Exchange Online
+Connect-ExchangeOnline -ShowBanner:$False
+ 
+#Get Existing Members of the Distribution List
+$DLMembers =  Get-DistributionGroupMember -Identity $GroupEmailID -ResultSize Unlimited | Select -Expand PrimarySmtpAddress
+ 
+#Import Distribution List Members from CSV
+Import-CSV $CSVFile -Header "UPN" | ForEach {
+    #Check if the Distribution List contains the particular user
+    If ($DLMembers -contains $_.UPN)
+    {
+        Write-host -f Yellow "User is already member of the Distribution List:"$_.UPN
+    }
+    Else
+    {        
+        Add-DistributionGroupMember -Identity $GroupEmailID -Member $_.UPN
+        Write-host -f Green "Added User to Distribution List:"$_.UPN
+    }
 }
