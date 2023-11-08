3373bb10416848dc3d1d7712a9211e28a59e586f
Bert de Zeeuw <38662369+Cavanite@users.noreply.github.com>
2023-11-08 14:28:23 +0100

    Update Install-Winget.ps1
    Added winget version controle before installation

--- PowerShell/Office365/Intune/Install-Winget.ps1
+++ PowerShell/Office365/Intune/Install-Winget.ps1
@@ -1,24 +1,27 @@
-# Check winget is installed
-if (-Not (Get-Command -Name winget -ErrorAction SilentlyContinue)) {
-    # Winget is not installed.
-    #script by Cavanite
-   $progressPreference = 'silentlyContinue'
-Write-Information "Downloading WinGet and its dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+# Check if winget is installed
+if (Get-Command "winget" -ErrorAction SilentlyContinue) {
+    # Get the version of winget
+    $wingetVersionOutput = winget -v
+    $wingetVersion = $null
 
- 
+    if ($wingetVersionOutput -match 'v(\d+\.\d+\.\d+)') {
+        $wingetVersion = [Version]$matches[1]
+        Write-Host "Winget is up to date."
+    }
 
-    # Installation Confirmation
-    if ($?) {
-        Write-Host "Winget installed succesfully"
-    } else {
-        Write-Host "Winget failed"
+    $requiredVersion = [Version]"1.6.2771"
+
+    # Compare the version with the required version
+    if ($wingetVersion -lt $requiredVersion) {
+        # Download the latest version of winget
+        Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile "winget-latest.appxbundle"
+        # Install the latest version of winget
+        Add-AppxPackage winget-latest.appxbundle
     }
 } else {
-    Write-Host "Winget is already installed"
+    Write-Output "winget is not installed."
+    #Install Winget
+    Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile "winget-latest.appxbundle"
+    # Install the latest version of winget
+    Add-AppxPackage winget-latest.appxbundle
 }
