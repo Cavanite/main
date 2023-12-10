5bdf935512c94cd29d22cbc6f9c0fd2709f0b9bb
Bert de Zeeuw <38662369+Cavanite@users.noreply.github.com>
2023-12-10 22:02:56 +0100

    Update Get-HardwareID.ps1

--- PowerShell/Office365/Intune/Get-HardwareID.ps1
+++ PowerShell/Office365/Intune/Get-HardwareID.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass Process 
 New-Item -Type Directory -Path "D:\HWID"
 Set-Location -Path "D:\HWID"
-$env:Path += "C:\Program Files\WindowsPowerShell\Scripts"Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+$env:Path += "C:\Program Files\WindowsPowerShell\Scripts"
 Install-Script -Name Get-WindowsAutopilotInfo -Force
 Get-WindowsAutopilotInfo -OutputFile .\$env:COMPUTERNAME.csv
