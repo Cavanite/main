607b164ecdce883c9e433a6b8de1d63a54c10967
Cavanite <bertdezeeuw@live.nl>
2024-01-30 16:27:08 +0100

    Added intune script.

---
+++ PowerShell/Office365/Intune/Get-Intune-Scripts.ps1
@@ -0,0 +1,50 @@
+#############################################
+#                                           #                       
+#          Script by Bert de Zeeuw          #
+#    visit https://github.com/Cavanite      # 
+#                                           #                       
+#############################################
+
+#this script will export all the scripts used in the intune admin center under section:
+#Devices -> Windows -> Scripts and Remdediations > Platform scripts.
+
+
+$m = Get-Module -Name Microsoft.Graph.Intune -ListAvailable
+if (-not $m)
+{
+    Install-Module NuGet -Force
+    Install-Module Microsoft.Graph.Intune
+}
+Import-Module Microsoft.Graph.Intune -Global
+
+Function Get-DeviceManagementScripts(){
+
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)][String] $FolderPath,
+        [Parameter(Mandatory=$false)][String] $FileName
+    )
+
+    $graphApiVersion = "Beta"
+    $graphUrl = "https://graph.microsoft.com/$graphApiVersion"
+
+    $result = Invoke-MSGraphRequest -Url "$graphUrl/deviceManagement/deviceManagementScripts" -HttpMethod GET
+
+    if ($FileName){
+        $scriptId = $result.value | Select-Object id,fileName | Where-Object -Property fileName -eq $FileName
+        $script = Invoke-MSGraphRequest -Url "$graphUrl/deviceManagement/deviceManagementScripts/$($scriptId.id)" -HttpMethod GET
+        [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($($script.scriptContent))) | Out-File -Encoding ASCII -FilePath $(Join-Path $FolderPath $($script.fileName))
+    }
+    else{
+        $scriptIds = $result.value | Select-Object id,fileName
+        foreach($scriptId in $scriptIds){
+            $script = Invoke-MSGraphRequest -Url "$graphUrl/deviceManagement/deviceManagementScripts/$($scriptId.id)" -HttpMethod GET
+            [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($($script.scriptContent))) | Out-File -Encoding ASCII -FilePath $(Join-Path $FolderPath $($script.fileName))
+        }
+    }
+}
+
+Connect-MSGraph | Out-Null
+
+Get-DeviceManagementScripts -FolderPath C:\temp
+#Get-DeviceManagementScripts -FolderPath C:\temp -FileName myScript.ps1
