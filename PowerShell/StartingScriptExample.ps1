1e6168934a8ca2728722947e8852c433d0230df5
Cavanite <bertdezeeuw@live.nl>
2024-05-07 10:11:49 +0200

    updates

--- PowerShell/StartingScriptExample.ps1
+++ PowerShell/StartingScriptExample.ps1
@@ -27,40 +27,11 @@ else {
     Start-Sleep -Seconds 2
 }
 
-Function Log-Message()
-{
-param
-    (
-    [Parameter(Mandatory=$true)] [string] $Message
-    )
-
-    Try {
-        #Get the current date
-        $LogDate = (Get-Date).tostring("dd-MM-yyyy")
-
-        #Set the log directory
-        $LogDir = "C:\Scripts"
-
-        #Frame Log File with Log Directory, Current Directory, and date
-        $LogFile = Join-Path -Path $LogDir -ChildPath ($LogDate + ".log")
-
-        #Add Content to the Log File
-        $TimeStamp = (Get-Date).toString("dd/MM/yyyy HH:mm:ss tt")
-        $Line = "$TimeStamp - $Message"
-        Add-content -Path $LogFile -Value $Line
-
-        Write-host -f Green "$Message"
-    }
-    Catch {
-        Write-host -f Red "Error:" $_.Exception.Message 
-    }
-}
 #######################################################################################################
 #######################################################################################################
-Log-Message "Script Started"
+
 
 #Your script here
 
 
-
-Log-Message "Script Completed"
+#######################################################################################################
