af4b2e8237533fd3e91e9e0e311ef19020c0ab98
Cavanite <bertdezeeuw@live.nl>
2025-04-28 11:01:48 +0200

    Updates

--- PowerShell/Devices/Install_Choco.ps1
+++ PowerShell/Devices/Install_Choco.ps1
@@ -5,23 +5,36 @@
 #                                           #                       
 #############################################
 
-# Check Choco is installed
-if (-Not (Get-Command -Name Choco -ErrorAction SilentlyContinue)) {
-    # Script by Cavanite
-    # Choco is not installed
-    # Script must a started with Administrator Privilages.
-          $progressPreference = 'silentlyContinue'
-Write-Information "Installing Chocolatey"
- 
+# Check if Chocolatey is installed
+if (-Not (Get-Command -Name choco -ErrorAction SilentlyContinue)) {
+    # Chocolatey is not installed
+    # Script must be started with Administrator Privileges.
+    $ProgressPreference = 'SilentlyContinue'
+    Write-Information "Installing Chocolatey..."
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
- 
-    # Install succesfull?
-    if ($?) {
-        Write-Host "Choco succesfully installed"
+    # Install Chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+    # Verify installation
+    if (Get-Command -Name choco -ErrorAction SilentlyContinue) {
+        Write-Host "Chocolatey successfully installed."
     } else {
-        Write-Host "Choco failed to install"
+        Write-Host "Chocolatey installation failed."
     }
 } else {
-    Write-Host "Choco is already installed."
+    # Chocolatey is installed, check version
+    $installedChocoVersion = [version](choco -v)
+    $requiredVersion = [version]"2.4.3"
+
+    if ($installedChocoVersion -lt $requiredVersion) {
+        Write-Host "Updating Chocolatey to version $requiredVersion..."
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+        Write-Host "Chocolatey updated successfully."
+    } else {
+        Write-Host "Chocolatey is up to date (Version: $installedChocoVersion)."
+    }
 }
